02637ab0d5e3f5903caa97fc29aba0ace6bfb9c6
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2026-03-18 03:24:28 +0530

    updated

--- Formula/lockknife.rb
+++ Formula/lockknife.rb
@@ -4,17 +4,15 @@
 class Lockknife < Formula
   include Language::Python::Virtualenv
 
-  desc "The Ultimate Android Security Research Tool"
+  desc "Ultimate Android Security Research Tool"
   homepage "https://lockknife.vercel.app"
-  license "GPL-3.0-only"
-  version "1.0.0"
-
   url "https://github.com/ImKKingshuk/LockKnife/releases/download/v1.0.0/lockknife-1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "GPL-3.0-only"
 
   depends_on arch: :arm64
-  depends_on "python@3.12"
   depends_on "rust" => :build
+  depends_on "python@3.12"
 
   def install
     virtualenv_install_with_resources
