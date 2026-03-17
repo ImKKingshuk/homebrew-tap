c0e454313b0a09e6e0fbc974f555dd056dc174cd
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2026-03-17 21:58:48 +0530

    updated

--- Formula/lockknife.rb
+++ Formula/lockknife.rb
@@ -9,13 +9,15 @@ class Lockknife < Formula
   license "GPL-3.0-only"
   version "1.0.0"
 
-  url "https://github.com/ImKKingshuk/LockKnife/releases/download/v1.0.0/lockknife-1.0.0-cp311-abi3-macosx_11_0_arm64.whl"
+  url "https://github.com/ImKKingshuk/LockKnife/releases/download/v1.0.0/lockknife-1.0.0-cp311-abi3-macosx_11_0_arm64.whl", using: :nounzip
   sha256 "586a547011739f496f93ab58d621103d032cc93d1d73a2796bb6da57ff28b48b"
 
   depends_on arch: :arm64
   depends_on "python@3.12"
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.12")
+    system libexec/"bin/pip", "install", "--no-deps", "--no-cache-dir", Dir["*.whl"].first
+    bin.install_symlink Dir[libexec/"bin/lockknife"]
   end
 end
